934200ae22492511d2a8e921fa44e2a12a76bd2b
Shaon Das <shaondas675@gmail.com>
2025-10-18 23:19:59 +0600

    commit

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/homeScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+   void initState() {
+      super.initState();
+
+      Future.delayed(Duration(seconds: 2), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      });
+    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,      
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/splash.png"),
+            fit: BoxFit.cover,          
+          ),
+        ),
+      ),
+    );
+  }
+}
